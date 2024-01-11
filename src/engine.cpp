234bacf9695ccc7b8aaf879d0b897294a1122a7e
nonlinearthink <2832203391@qq.com>
2024-01-11 19:15:43 +0800

    google style code

--- src/engine.cpp
+++ src/engine.cpp
@@ -3,11 +3,115 @@
 using namespace ScratchEngine;
 using namespace Microsoft::WRL;
 
+Engine::~Engine() { Shutdown(); }
+
+bool Engine::Initialize() {
+    if (main_device_.Get()) {
+        Shutdown();
+    }
+
+    UINT dxgi_factory_flags{0};
+
+#ifdef _DEBUG
+    // Enable the D3D12 debug layer.
+    {
+        ComPtr<ID3D12Debug> debugController;
+        if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)))) {
+            debugController->EnableDebugLayer();
+        }
+
+        // Enable DXGI Factory debug.
+        dxgi_factory_flags |= DXGI_CREATE_FACTORY_DEBUG;
+    }
+#endif
+
+    HRESULT hr{S_OK};
+
+    THROW_IF_FAILED(
+        hr = CreateDXGIFactory2(dxgi_factory_flags, IID_PPV_ARGS(&factory_)));
+    if (FAILED(hr)) {
+        return HandleInitializeFailed();
+    }
+
+    // Get a suitable adapter.
+    ComPtr<IDXGIAdapter4> main_adapter;
+    main_adapter.Attach(GetSuitableAdapter());
+    if (!main_adapter) {
+        return HandleInitializeFailed();
+    }
+
+    // Check the maximum feature level.
+    D3D_FEATURE_LEVEL maximum_feature_level{
+        GetMaxFeatureLevel(main_adapter.Get())};
+    if (maximum_feature_level <= MINIMUM_D3D_FEATURN_LEVEL) {
+        return HandleInitializeFailed();
+    }
+
+    THROW_IF_FAILED(hr = D3D12CreateDevice(main_adapter.Get(),
+                                           maximum_feature_level,
+                                           IID_PPV_ARGS(&main_device_)));
+    if (FAILED(hr)) {
+        return HandleInitializeFailed();
+    }
+
+    // NAME_D3D12_OBJECT(main_device_, "Main D3D12 Device");
+
+#ifdef _DEBUG
+    {
+        ComPtr<ID3D12InfoQueue> info_queue;
+        THROW_IF_FAILED(
+            hr = main_device_->QueryInterface(IID_PPV_ARGS(&info_queue)));
+        // Enable severities.
+        info_queue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_CORRUPTION, true);
+        info_queue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR, true);
+        info_queue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_WARNING, true);
+    }
+#endif
+
+    return true;
+}
+
+void Engine::Shutdown() {
+    auto factory = factory_.Get();
+    ReleaseResource(factory);
+
+#ifdef _DEBUG
+    {
+        ComPtr<ID3D12InfoQueue> info_queue;
+        THROW_IF_FAILED(
+            main_device_->QueryInterface(IID_PPV_ARGS(&info_queue)));
+        // Disable severities.
+        info_queue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_CORRUPTION,
+                                       false);
+        info_queue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR, false);
+        info_queue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_WARNING, false);
+
+        ComPtr<ID3D12DebugDevice2> debugDevice;
+        THROW_IF_FAILED(
+            main_device_->QueryInterface(IID_PPV_ARGS(&debugDevice)));
+        auto device = main_device_.Get();
+        ReleaseResource(device);
+        // Report live objects that causes memory leaks when exit.
+        THROW_IF_FAILED(debugDevice->ReportLiveDeviceObjects(
+            D3D12_RLDO_SUMMARY | D3D12_RLDO_DETAIL |
+            D3D12_RLDO_IGNORE_INTERNAL));
+    }
+#else
+    auto device = main_device_.Get();
+    ReleaseResource(device);
+#endif
+}
+
+bool Engine::HandleInitializeFailed() {
+    Shutdown();
+    return false;
+}
+
 IDXGIAdapter4 *Engine::GetSuitableAdapter() {
     IDXGIAdapter4 *adapter{nullptr};
 
     // Get adapters in descending order of preference.
-    for (UINT i{0}; m_factory->EnumAdapterByGpuPreference(
+    for (UINT i{0}; factory_->EnumAdapterByGpuPreference(
                         i, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE,
                         IID_PPV_ARGS(&adapter)) != DXGI_ERROR_NOT_FOUND;
          ++i) {
@@ -41,102 +145,4 @@ D3D_FEATURE_LEVEL Engine::GetMaxFeatureLevel(IDXGIAdapter4 *adapter) {
                                                 &feature_levels_info,
                                                 sizeof(feature_levels_info)));
     return feature_levels_info.MaxSupportedFeatureLevel;
-}
-
-bool Engine::InitializeFailed() {
-    Shutdown();
-    return false;
-}
-
-bool Engine::Initialize() {
-    if (m_device.Get()) {
-        Shutdown();
-    }
-
-    UINT dxgi_factory_flags{0};
-
-#ifdef _DEBUG
-    // Enable the D3D12 debug layer.
-    {
-        ComPtr<ID3D12Debug> debugController;
-        if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)))) {
-            debugController->EnableDebugLayer();
-        }
-
-        // Enable DXGI Factory debug.
-        dxgi_factory_flags |= DXGI_CREATE_FACTORY_DEBUG;
-    }
-#endif
-
-    HRESULT hr{S_OK};
-
-    THROW_IF_FAILED(
-        hr = CreateDXGIFactory2(dxgi_factory_flags, IID_PPV_ARGS(&m_factory)));
-    if (FAILED(hr)) {
-        return InitializeFailed();
-    }
-
-    // Get a suitable adapter.
-    ComPtr<IDXGIAdapter4> main_adapter;
-    main_adapter.Attach(GetSuitableAdapter());
-    if (!main_adapter) {
-        return InitializeFailed();
-    }
-
-    // Check the maximum feature level.
-    D3D_FEATURE_LEVEL maximum_feature_level{
-        GetMaxFeatureLevel(main_adapter.Get())};
-    if (maximum_feature_level <= MINIMUM_D3D_FEATURN_LEVEL) {
-        return InitializeFailed();
-    }
-
-    THROW_IF_FAILED(D3D12CreateDevice(main_adapter.Get(), maximum_feature_level,
-                                      IID_PPV_ARGS(&m_device)));
-    if (FAILED(hr)) {
-        return InitializeFailed();
-    }
-
-    // NAME_D3D12_OBJECT(m_device, "Main D3D12 Device");
-
-#ifdef _DEBUG
-    {
-        ComPtr<ID3D12InfoQueue> info_queue;
-        THROW_IF_FAILED(m_device->QueryInterface(IID_PPV_ARGS(&info_queue)));
-        // Enable severities.
-        info_queue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_CORRUPTION, true);
-        info_queue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR, true);
-        info_queue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_WARNING, true);
-    }
-#endif
-
-    return true;
-}
-
-void Engine::Shutdown() {
-    auto factory = m_factory.Get();
-    ReleaseResource(factory);
-
-#ifdef _DEBUG
-    {
-        ComPtr<ID3D12InfoQueue> info_queue;
-        THROW_IF_FAILED(m_device->QueryInterface(IID_PPV_ARGS(&info_queue)));
-        // Disable severities.
-        info_queue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_CORRUPTION,
-                                       false);
-        info_queue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR, false);
-        info_queue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_WARNING, false);
-
-        ComPtr<ID3D12DebugDevice2> debug_device;
-        THROW_IF_FAILED(m_device->QueryInterface(IID_PPV_ARGS(&debug_device)));
-        auto device = m_device.Get();
-        ReleaseResource(device);
-        // Report live objects that causes memory leaks when exit.
-        THROW_IF_FAILED(debug_device->ReportLiveDeviceObjects(
-            D3D12_RLDO_SUMMARY | D3D12_RLDO_DETAIL |
-            D3D12_RLDO_IGNORE_INTERNAL));
-    }
-#else
-    auto device = m_device.Get();
-    ReleaseResource(device);
-#endif
 }
--- src/engine.h
+++ src/engine.h
@@ -17,23 +17,22 @@ constexpr D3D_FEATURE_LEVEL MINIMUM_D3D_FEATURN_LEVEL{D3D_FEATURE_LEVEL_11_0};
 
 class Engine {
 public:
+    ~Engine();
+
     // Initialize the engine.
     bool Initialize();
-
     // Shutdown the engine.
     void Shutdown();
 
 private:
-    Microsoft::WRL::ComPtr<IDXGIFactory7> m_factory{nullptr};
-    Microsoft::WRL::ComPtr<ID3D12Device8> m_device{nullptr};
-
-    // Get a suitable adapter that supports minimum feature level.
-    IDXGIAdapter4 *GetSuitableAdapter();
-
-    // Get the maximum feature level supported by the device.
-    D3D_FEATURE_LEVEL GetMaxFeatureLevel(IDXGIAdapter4 *adapter);
+    Microsoft::WRL::ComPtr<IDXGIFactory7> factory_{nullptr};
+    Microsoft::WRL::ComPtr<ID3D12Device8> main_device_{nullptr};
 
     // Initialize fail
-    bool InitializeFailed();
+    bool HandleInitializeFailed();
+    // Get a suitable adapter that supports minimum feature level.
+    IDXGIAdapter4 *GetSuitableAdapter();
+    // Get the maximum feature level supported by the device.
+    D3D_FEATURE_LEVEL GetMaxFeatureLevel(IDXGIAdapter4 *adapter);
 };
 }  // namespace ScratchEngine
